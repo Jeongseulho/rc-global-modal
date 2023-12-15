71071d0bbf52a6fc543c2d8d9932c4b87948b269
Jeong seulho <wjdtmfgh@gmail.com>
2023-12-15 19:18:09 +0900

    library dev setting with emotion

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import * as path from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'rc-global-modal',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+    commonjsOptions: {
+      esmExternals: ['react'],
+    },
+  },
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+    }),
+    dts(),
+  ],
+});
