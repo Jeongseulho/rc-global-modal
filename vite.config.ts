36b95d5a4b073d8278c10a8a63b8a9f81c2687ad
Jeong seulho <wjdtmfgh@gmail.com>
2024-02-18 22:25:28 +0900

    build : remove format umd and cjs

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       entry: path.resolve(__dirname, 'src/lib/index.ts'),
       name: 'rc-global-modal',
       fileName: 'index',
-      formats: ['es', 'umd', 'cjs'],
+      formats: ['es'],
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
