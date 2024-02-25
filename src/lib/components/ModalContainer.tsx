d4d7eb2764ce5b17761dc8f050de88869a8c8c85
Jeong seulho <wjdtmfgh@gmail.com>
2024-02-25 19:31:59 +0900

    feat : add ref props

--- src/lib/components/ModalContainer.tsx
+++ src/lib/components/ModalContainer.tsx
@@ -4,12 +4,14 @@ interface Props {
   children: React.ReactNode;
   modalContainerClassName?: string;
   modalContainerStyle?: React.CSSProperties;
+  modalContainerRef?: React.RefObject<HTMLDivElement>;
 }
 
 const ModalContainer = ({
   children,
   modalContainerClassName,
   modalContainerStyle,
+  modalContainerRef,
 }: Props) => {
   return (
     <div
@@ -20,6 +22,7 @@ const ModalContainer = ({
         ...modalContainerStyle,
       })}
       className={modalContainerClassName}
+      ref={modalContainerRef}
     >
       {children}
     </div>
